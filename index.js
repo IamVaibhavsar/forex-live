89bfd58d21afd0d165e5102a1cd05adf559f0435
Vaibhav Bhavsar <Vaibhavbhavsar007@gmail.com>
2020-06-11 16:39:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config()
+
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 9000;
+
+const cryptoroutes = require('./routes/crypto');
+const currencyroutes = require('./routes/currency');
+const homecontroller = require('./Controllers/homecontol')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('./public'))
+
+app.set('views','views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', homecontroller.homestuff)
+
+app.use('/currency', currencyroutes)
+
+app.use('/crypto',cryptoroutes);
+
+app.use(homecontroller.errorstuff)
+
+//app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Forex App is up and running at https://localhost:${PORT}`)
+})
